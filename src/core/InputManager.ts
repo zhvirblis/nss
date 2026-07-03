03e49f266e65001f78af714aac3877b44b4e19d4
Yuri Zhvirblis <11149023+zhvirblis@users.noreply.github.com>
2026-07-03 19:18:58 +0300

    gamepad support

--- src/core/InputManager.ts
+++ src/core/InputManager.ts
@@ -59,30 +59,72 @@ export class InputManager {
     }
   }
 
+  // --- Gamepad support ---
+  private gamepadIndex: number | null = null;
+
+  private onGamepadConnected = (e: GamepadEvent): void => {
+    this.gamepadIndex = e.gamepad.index;
+  };
+
+  private onGamepadDisconnected = (e: GamepadEvent): void => {
+    if (this.gamepadIndex === e.gamepad.index) {
+      this.gamepadIndex = null;
+    }
+  };
+
+  private gpLeft = false;
+  private gpRight = false;
+  private gpUp = false;
+  private gpDown = false;
+  private gpFire = false;
+  private gpMenu = false;
+
+  private pollGamepad(): void {
+    if (this.gamepadIndex === null) return;
+    const gamepads = navigator.getGamepads();
+    const gp = gamepads[this.gamepadIndex];
+    if (!gp) return;
+
+    const btns = gp.buttons;
+    this.gpLeft = !!(btns[14]?.pressed || gp.axes[0] < -0.5);
+    this.gpRight = !!(btns[15]?.pressed || gp.axes[0] > 0.5);
+    this.gpUp = !!(btns[12]?.pressed || gp.axes[1] < -0.5);
+    this.gpDown = !!(btns[13]?.pressed || gp.axes[1] > 0.5);
+    this.gpUp = this.gpUp || !!btns[0]?.pressed;
+    this.gpFire = !!btns[3]?.pressed;
+    this.gpMenu = !!(btns[8]?.pressed || btns[9]?.pressed);
+  }
+
   get effective(): InputState {
+    this.pollGamepad();
     return {
-      left: this.state.left || this.virtLeft,
-      right: this.state.right || this.virtRight,
-      up: this.state.up || this.virtUp,
-      down: this.state.down || this.virtDown,
-      fire: this.state.fire || this.virtFire,
-      menu: this.state.menu || this.virtMenu,
+      left: this.state.left || this.virtLeft || this.gpLeft,
+      right: this.state.right || this.virtRight || this.gpRight,
+      up: this.state.up || this.virtUp || this.gpUp,
+      down: this.state.down || this.virtDown || this.gpDown,
+      fire: this.state.fire || this.virtFire || this.gpFire,
+      menu: this.state.menu || this.virtMenu || this.gpMenu,
     };
   }
 
   attach(): void {
     window.addEventListener('keydown', this.onKeyDown);
     window.addEventListener('keyup', this.onKeyUp);
+    window.addEventListener('gamepadconnected', this.onGamepadConnected);
+    window.addEventListener('gamepaddisconnected', this.onGamepadDisconnected);
   }
 
   detach(): void {
     window.removeEventListener('keydown', this.onKeyDown);
     window.removeEventListener('keyup', this.onKeyUp);
+    window.removeEventListener('gamepadconnected', this.onGamepadConnected);
+    window.removeEventListener('gamepaddisconnected', this.onGamepadDisconnected);
   }
 
   clearMenu(): void {
     this.state.menu = false;
     this.menuPressed = false;
     this.virtMenu = false;
+    this.gpMenu = false;
   }
 }
